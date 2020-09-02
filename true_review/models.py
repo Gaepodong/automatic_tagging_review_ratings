cfaa9e4ab62f2e0eaf7fb97b871161178fc57196
humblEgo <injun.woo30000@gmail.com>
2020-09-02 17:51:43 +0900

    MODIFY: Movie table to Movies table

--- true_review/models.py
+++ true_review/models.py
@@ -12,19 +12,19 @@ class Movies(db.Model):
 class Reviews(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     movie_id = db.Column(db.Integer, db.ForeignKey(
-        'movie.id', ondelete='CASCADE'))
+        'movies.id', ondelete='CASCADE'))
     text_rank = db.Column(db.Float, nullable=True)
     content = db.Column(db.Text(), nullable=False)
     movie_rating = db.Column(db.Integer, nullable=False)
     emotion = db.Column(db.Boolean, nullable=False)
-    reviews = db.relationship('Movie', backref=db.backref('review_set'))
+    reviews = db.relationship('Movies', backref=db.backref('review_set'))
 
 
 class Comments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     movie_id = db.Column(db.Integer, db.ForeignKey(
-        'movie.id', ondelete='CASCADE'))
-    movie = db.relationship('Movie', backref=db.backref('comment_set'))
+        'movies.id', ondelete='CASCADE'))
+    movie = db.relationship('Movies', backref=db.backref('comment_set'))
     content = db.Column(db.Text(), nullable=False)
     movie_rating = db.Column(db.Integer, nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
