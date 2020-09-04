57d97b550320ba74edcf7a5533ee8f131c55fe7c
humblEgo <injun.woo30000@gmail.com>
2020-09-04 19:21:20 +0900

    ADD: connection between nlp_anlaysis server

--- true_review/views/comment_views.py
+++ true_review/views/comment_views.py
@@ -3,6 +3,9 @@ from datetime import datetime
 from flask import Blueprint, url_for, request, render_template
 from werkzeug.utils import redirect
 
+import requests as req
+import json
+
 from true_review import db
 from true_review.models import Comments, Movies, Reviews
 
@@ -19,13 +22,18 @@ def create(movie_id):
     form = CommentForm()
     if request.method == 'POST' and form.validate_on_submit():
         # TODO rating, emotion_percent 가져오기
-        # emotion_percent = getEmotion(comment.content)
-        # movie_rating = getRating(comment.content)
         emotion_percent = 0.8
         movie_rating = 10
+        # response = req.post('http://13.209.65.40:59274/test',
+        #                     data={'text': comment.content})
+        # temp = response.json()
+        # emotion_percent = temp['emotion_percent']
+        # movie_rating = temp['movie_rating']
+
         comment = Comments(movie_id=movie_id, content=form.content.data,
                            movie_rating=movie_rating, create_date=datetime.now(), emotion_percent=emotion_percent)
         movie.comment_set.append(comment)
+
         db.session.commit()
         # return redirect(url_for('movies.movie', movie_code=movie.code, isFirstRender=0))
         return render_template('movies/movie_detail.html', movie=movie, form=form, isFirstRender=0, review_list=review_list, comment=comment)
